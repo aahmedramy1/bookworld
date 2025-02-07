2002165c62e4295e20a35fd881c7f6e3d1b5993b
ahmedramy <ahmedramy.e404@gmail.com>
2025-02-07 12:22:20 +0200

    feat: GenericList

---
+++ src/components/GenericList.jsx
@@ -0,0 +1,173 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TablePagination,
+    TableRow,
+    Checkbox,
+    Button,
+    Paper
+} from '@mui/material';
+
+const GenericList = ({ data, columns, onSelectionChange }) => {
+    const [selected, setSelected] = useState([]);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+
+
+    const getRowId = (row, index) => row.id ?? index;
+
+
+    const handleSelectAllClick = (event) => {
+        if (event.target.checked) {
+            const newSelectedIds = data.map((row, index) => getRowId(row, index));
+            setSelected(newSelectedIds);
+            if (onSelectionChange) {
+                onSelectionChange(data);
+            }
+        } else {
+            setSelected([]);
+            if (onSelectionChange) {
+                onSelectionChange([]);
+            }
+        }
+    };
+
+    const handleClick = (event, row, overallIndex) => {
+        const id = getRowId(row, overallIndex);
+        const selectedIndex = selected.indexOf(id);
+        let newSelected = [];
+
+        if (selectedIndex === -1) {
+            newSelected = [...selected, id];
+        } else {
+            newSelected = selected.filter(item => item !== id);
+        }
+        setSelected(newSelected);
+
+        const selectedRows = data.filter((r, i) => newSelected.includes(getRowId(r, i)));
+        if (onSelectionChange) {
+            onSelectionChange(selectedRows);
+        }
+    };
+
+    const isSelected = (row, overallIndex) => selected.indexOf(getRowId(row, overallIndex)) !== -1;
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const paginatedData = data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
+
+    return (
+        <Paper>
+            <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell padding="checkbox">
+                                <Checkbox
+                                    indeterminate={selected.length > 0 && selected.length < data.length}
+                                    checked={data.length > 0 && selected.length === data.length}
+                                    onChange={handleSelectAllClick}
+                                />
+                            </TableCell>
+                            {/* Render headers for each column */}
+                            {columns.map((column, colIndex) => (
+                                <TableCell key={colIndex}>
+                                    {column.headerName}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {paginatedData.map((row, rowIndex) => {
+                            const overallIndex = page * rowsPerPage + rowIndex;
+                            const isItemSelected = isSelected(row, overallIndex);
+
+                            return (
+                                <TableRow
+                                    hover
+                                    key={getRowId(row, overallIndex)}
+                                    role="checkbox"
+                                    aria-checked={isItemSelected}
+                                    selected={isItemSelected}
+                                >
+                                    <TableCell padding="checkbox">
+                                        <Checkbox
+                                            checked={isItemSelected}
+                                            onChange={(event) => handleClick(event, row, overallIndex)}
+                                        />
+                                    </TableCell>
+
+                                    {columns.map((column, colIndex) => (
+                                        <TableCell key={colIndex}>
+                                            {column.actions ? (
+                                                column.actions.map((action, actionIndex) => (
+                                                    <Button
+                                                        key={actionIndex}
+                                                        onClick={() => action.onClick(row)}
+                                                        size="small"
+                                                        variant="outlined"
+                                                        style={{ marginRight: 4 }}
+                                                    >
+                                                        {action.label}
+                                                    </Button>
+                                                ))
+                                            ) : column.renderCell ? (
+                                                // If a custom renderCell function is provided.
+                                                column.renderCell(row[column.field], row)
+                                            ) : (
+                                                // Default: display the data field.
+                                                row[column.field]
+                                            )}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            <TablePagination
+                component="div"
+                count={data.length}
+                page={page}
+                onPageChange={handleChangePage}
+                rowsPerPage={rowsPerPage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                rowsPerPageOptions={[5, 10, 25]}
+            />
+        </Paper>
+    );
+};
+
+GenericList.propTypes = {
+    data: PropTypes.array.isRequired,
+    columns: PropTypes.arrayOf(
+        PropTypes.shape({
+            field: PropTypes.string,
+            headerName: PropTypes.string,
+            renderCell: PropTypes.func,
+            actions: PropTypes.arrayOf(
+                PropTypes.shape({
+                    label: PropTypes.string.isRequired,
+                    onClick: PropTypes.func.isRequired,
+                })
+            )
+        })
+    ).isRequired,
+    onSelectionChange: PropTypes.func,
+};
+
+export default GenericList;
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,9 +1,49 @@
+import React, { useState } from 'react';
+import GenericList from '../components/GenericList';
+import {useSelector} from "react-redux";
+
 const Books = () => {
+    const [selectedRows, setSelectedRows] = useState([]);
+    const data = useSelector(state => state.books);
+
+    const columns = [
+        {field: 'id', headerName: 'Book ID'},
+        { field: 'name', headerName: 'Name' },
+        {field: 'pageCount', headerName: 'Pages'},
+        {field: 'authorId', headerName: 'Author'},
+        {
+            headerName: 'Actions',
+            actions: [
+                {
+                    label: 'Edit',
+                    onClick: (row) => {
+                        console.log('Edit clicked for:', row);
+                    }
+                },
+                {
+                    label: 'Delete',
+                    onClick: (row) => {
+                        console.log('Delete clicked for:', row);
+                    }
+                }
+            ]
+        }
+    ];
+
+    const handleSelectionChange = (newSelectedRows) => {
+        setSelectedRows(newSelectedRows);
+        console.log('Selected rows:', newSelectedRows);
+    };
+
     return (
         <div>
-            <h1>Books</h1>
+            <GenericList
+                data={data}
+                columns={columns}
+                onSelectionChange={handleSelectionChange}
+            />
         </div>
     );
-}
+};
 
 export default Books;
