86f50a962a31ab06c28434d7648ab795ebf8ad93
ahmedramy <ahmedramy.e404@gmail.com>
2025-02-07 15:07:00 +0200

    feat: Persist store

--- src/index.js
+++ src/index.js
@@ -3,21 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
-import store from "./redux/store";
+import store, {persistor} from "./redux/store";
 import {CssBaseline, ThemeProvider} from "@mui/material";
 import theme from "./theme";
 import {BrowserRouter} from "react-router-dom";
+import {PersistGate} from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <Provider store={store}>
-          <ThemeProvider theme={theme}>
-              <CssBaseline />
-              <BrowserRouter>
-                  <App />
-              </BrowserRouter>
-          </ThemeProvider>
+          <PersistGate loading={null} persistor={persistor}>
+              <ThemeProvider theme={theme}>
+                  <CssBaseline />
+                  <BrowserRouter>
+                      <App />
+                  </BrowserRouter>
+              </ThemeProvider>
+          </PersistGate>
       </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // Uses localStorage
 import booksReducer from "./slices/booksSlice";
 import authorsReducer from "./slices/authorSlice";
 import storesReducer from "./slices/storesSlice";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    books: booksReducer,
+    authors: authorsReducer,
+    stores: storesReducer,
+});
+
+const persistConfig = {
+    key: "root", // Root key for storage
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        books: booksReducer,
-        authors: authorsReducer,
-        stores: storesReducer,
-    },
+    reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
 
 export default store;
